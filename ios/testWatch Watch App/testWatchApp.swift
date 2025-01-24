7477aaadf1916394bae6d7fde64b0b52c4a870e5
Martin Rost <martin.rost@5w-verlag.de>
2025-01-24 11:46:41 +0100

    adding splashview and apple watch

---
+++ ios/testWatch Watch App/testWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  testWatchApp.swift
+//  testWatch Watch App
+//
+//  Created by Martin Rost on 24.01.25.
+//
+
+import SwiftUI
+
+@main
+struct testWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
